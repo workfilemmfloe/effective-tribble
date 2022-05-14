aaeccce8fb808e21a82f5906726d6a0afc96edf7
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 18:37:06 -0400

    Initialize tsvetkov/172

---
+++ compiler/testData/loadJava/compiledKotlin/fromLoadJava/kotlinSignature/propagation/return/HalfSubstitutedTypeParameters.kt
@@ -0,0 +1,18 @@
+// FULL_JDK
+
+package test
+
+public interface HalfSubstitutedTypeParameters {
+
+    public interface TrickyList<X, E>: MutableList<E> {}
+
+    public interface Super {
+        public fun foo(): MutableList<String?>
+
+        public fun dummy() // to avoid loading as SAM interface
+    }
+
+    public interface Sub: Super {
+        override fun foo(): TrickyList<Int, String?>
+    }
+}
