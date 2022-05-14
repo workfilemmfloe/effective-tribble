9bc46599aef064a32e5ba2b39b5515e097aaa2be
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 19:22:27 -0400

    Initialize 1.2.0_172

---
+++ libraries/stdlib/test/collections/IteratorsTest.kt
@@ -0,0 +1,15 @@
+package test.collections
+
+import kotlin.test.*
+import org.junit.Test
+
+class IteratorsTest {
+    @Test fun iterationOverIterator() {
+        val c = listOf(0, 1, 2, 3, 4, 5)
+        var s = ""
+        for (i in c.iterator()) {
+            s = s + i.toString()
+        }
+        assertEquals("012345", s)
+    }
+}
