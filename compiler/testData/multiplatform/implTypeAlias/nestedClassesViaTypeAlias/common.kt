301afc8b4c492e9ce1c54112cb9d68d0224e3b34
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 19:37:20 -0400

    Initialize 1.1.50

---
+++ compiler/testData/multiplatform/implTypeAlias/nestedClassesViaTypeAlias/common.kt
@@ -0,0 +1,3 @@
+header class ByTypeAlias {
+    interface Nested
+}
---
+++ idea/testData/multiModuleQuickFix/enum/header/header.kt
@@ -0,0 +1,9 @@
+// "Create header class implementation for platform JS" "true"
+
+header enum class <caret>MyEnum {
+    FIRST,
+    SECOND,
+    LAST;
+
+    val num: Int
+}
