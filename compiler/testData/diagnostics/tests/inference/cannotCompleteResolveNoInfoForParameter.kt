da4cd29023d01f8886d08e19df4ea404da05c072
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 19:23:42 -0400

    Initialize 1.1.50_172

---
+++ compiler/testData/diagnostics/tests/inference/cannotCompleteResolveNoInfoForParameter.kt
@@ -0,0 +1,10 @@
+package f
+
+fun <T> f(<!UNUSED_PARAMETER!>i<!>: Int, <!UNUSED_PARAMETER!>c<!>: Collection<T>): List<T> {throw Exception()}
+fun <T> f(<!UNUSED_PARAMETER!>a<!>: Any, <!UNUSED_PARAMETER!>l<!>: List<T>): Collection<T> {throw Exception()}
+
+fun <T> test(<!UNUSED_PARAMETER!>l<!>: List<T>) {
+    <!CANNOT_COMPLETE_RESOLVE!>f<!>(1, <!TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>emptyList<!>())
+}
+
+fun <T> emptyList(): List<T> {throw Exception()}
