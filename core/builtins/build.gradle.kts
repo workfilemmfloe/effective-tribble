5e9380c8abf4b5208a763dab34827f7672e94ec5
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 23:34:14 -0400

    Initialize dsavvinov/additional-tests-for-conditional-jumps

---
+++ core/builtins/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.gradle.jvm.tasks.Jar
+import java.io.File
+
+plugins {
+    base
+}
+
+val builtinsSrc = fileFrom(rootDir, "core", "builtins", "src")
+val builtinsNative = fileFrom(rootDir, "core", "builtins", "native")
+// TODO: rewrite dependent projects on using build results instead of the fixed location
+val builtinsSerialized = File(rootProject.extra["distDir"].toString(), "builtins")
+
+val builtins by configurations.creating
+
+val clean by tasks.getting {
+    doLast {
+        delete(builtinsSerialized)
+    }
+}
+
+val serialize by tasks.creating(JavaExec::class) {
+    val outDir = builtinsSerialized
+    val inDirs = arrayOf(builtinsSrc, builtinsNative)
+    inDirs.forEach { inputs.dir(it) }
+    outputs.dir(outDir)
+
+    classpath(rootProject.buildscript.configurations["bootstrapCompilerClasspath"])
+    main = "org.jetbrains.kotlin.serialization.builtins.RunKt"
+
+    doFirst {
+        // Reassign to remove debugger options. Workaround for IDEA-200192.
+        jvmArgs = listOf("-Didea.io.use.nio2=true")
+    }
+
+    args(outDir, *inDirs)
+}
+
+val builtinsJar by task<Jar> {
+    dependsOn(serialize)
+    from(builtinsSerialized) { include("kotlin/**") }
+    baseName = "platform-builtins"
+    destinationDir = File(buildDir, "libs")
+}
+
+
+val assemble by tasks.getting {
+    dependsOn(serialize)
+}
+
+artifacts.add(builtins.name, builtinsJar)
