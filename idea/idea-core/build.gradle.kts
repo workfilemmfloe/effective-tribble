840a261b5afb9e7205928ee18c2febadb6c4e44a
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 20:56:34 -0400

    Initialize 1.2.20_as30

---
+++ idea/idea-core/build.gradle.kts
@@ -0,0 +1,31 @@
+apply { plugin("kotlin") }
+
+dependencies {
+    compile(projectDist(":kotlin-stdlib"))
+    compileOnly(project(":kotlin-reflect-api"))
+    compile(project(":core:descriptors"))
+    compile(project(":core:descriptors.jvm"))
+    compile(project(":compiler:frontend"))
+    compile(project(":compiler:frontend.java"))
+    compile(project(":compiler:frontend.script"))
+    compile(project(":compiler:light-classes"))
+    compile(project(":compiler:util"))
+    compile(project(":j2k"))
+    compile(project(":idea:ide-common"))
+    compile(project(":idea:idea-jps-common"))
+    compile(project(":plugins:android-extensions-compiler"))
+    compile(ideaSdkCoreDeps("intellij-core", "util"))
+    compile(ideaSdkDeps("openapi", "idea"))
+    compile(ideaPluginDeps("gradle-tooling-api", "gradle", plugin = "gradle"))
+    //compileOnly(preloadedDeps("uast-common"))
+    compile(preloadedDeps("kotlinx-coroutines-core", "kotlinx-coroutines-jdk8"))
+}
+
+sourceSets {
+    "main" {
+        projectDefault()
+        java.srcDir("../idea-analysis/src")
+        resources.srcDir("../idea-analysis/src").apply { include("**/*.properties") }
+    }
+    "test" {}
+}
---
+++ plugins/sam-with-receiver/sam-with-receiver-cli/build.gradle.kts
@@ -0,0 +1,48 @@
+
+description = "Kotlin SamWithReceiver Compiler Plugin"
+
+apply { plugin("kotlin") }
+
+dependencies {
+    compileOnly(project(":compiler:frontend"))
+    compileOnly(project(":compiler:frontend.java"))
+    compileOnly(project(":compiler:plugin-api"))
+    runtime(projectRuntimeJar(":kotlin-compiler"))
+    runtime(projectDist(":kotlin-stdlib"))
+    runtime(projectDist(":kotlin-reflect"))
+
+    testCompile(project(":compiler:backend"))
+    testCompile(project(":compiler:cli"))
+    testCompile(project(":compiler:tests-common"))
+    testCompile(projectTests(":compiler:tests-common"))
+    testCompile(commonDep("junit:junit"))
+    testRuntime(ideaSdkDeps("resources"))
+    testRuntime(ideaSdkDeps("*.jar"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" { projectDefault() }
+}
+
+val jar = runtimeJar {
+    from(fileTree("$projectDir/src")) { include("META-INF/**") }
+}
+sourcesJar()
+javadocJar()
+testsJar {}
+
+publish()
+
+dist {
+    rename("kotlin-", "")
+}
+
+ideaPlugin {
+    from(jar)
+    rename("^kotlin-", "")
+}
+
+projectTest {
+    workingDir = rootDir
+}
