b14f83f71102057e0c324a7fb35d6a9361892f23
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 22:25:07 -0400

    Initialize ssa_ir_types

---
+++ compiler/tests-common/tests/org/jetbrains/kotlin/test/ConfigurationKind.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test
+
+enum class ConfigurationKind(
+        val withRuntime: Boolean = false,
+        val withMockRuntime: Boolean = false,
+        val withReflection: Boolean = false,
+        val withCoroutines: Boolean = false
+) {
+    /** JDK without any kotlin runtime */
+    JDK_NO_RUNTIME(),
+    /** JDK + light mock kotlin runtime */
+    JDK_ONLY(withMockRuntime = true),
+    /** JDK + kotlin runtime but without reflection */
+    NO_KOTLIN_REFLECT(withRuntime = true),
+    /** JDK + kotlin runtime + coroutines */
+    WITH_COROUTINES(withCoroutines = true, withRuntime = true),
+    /** JDK + kotlin runtime + kotlin reflection */
+    WITH_REFLECT(withRuntime = true, withReflection = true),
+    /** JDK + kotlin runtime + kotlin reflection + coroutines*/
+    ALL(withRuntime = true, withReflection = true, withCoroutines = true)
+}
