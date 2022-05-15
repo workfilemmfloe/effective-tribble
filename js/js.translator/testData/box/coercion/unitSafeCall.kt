af5619565c4b5032a89b7334da5b9d66bd7c0a66
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 22:02:33 -0400

    Initialize erokhins/sam

---
+++ js/js.translator/testData/box/coercion/unitSafeCall.kt
@@ -0,0 +1,15 @@
+// IGNORE_BACKEND: JS_IR
+// EXPECTED_REACHABLE_NODES: 1112
+class C {
+    fun foo() {}
+}
+
+fun box(): String {
+    val a: C? = C()
+    val b: C? = null
+
+    if (a?.foo() != Unit) return "fail1: ${a?.foo()}"
+    if (b?.foo() != null) return "fail2: ${b?.foo()}"
+
+    return "OK"
+}
