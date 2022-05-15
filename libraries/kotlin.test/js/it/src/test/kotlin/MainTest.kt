c5e8bee97aa69b0d336b4815b80d85925996e1af
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 20:24:42 -0400

    Initialize 1.1.60_163

---
+++ libraries/kotlin.test/js/it/src/test/kotlin/MainTest.kt
@@ -0,0 +1,24 @@
+import kotlin.test.*
+
+
+class SimpleTest {
+
+    @Test fun testFoo() {
+        assertNotEquals(10, foo())
+    }
+
+    @Test fun testBar() {
+        assertEquals(10, foo())
+    }
+
+    @Ignore @Test fun testFooWrong() {
+        assertEquals(20, foo())
+    }
+
+}
+
+@Ignore
+class TestTest {
+    @Test fun emptyTest() {
+    }
+}
