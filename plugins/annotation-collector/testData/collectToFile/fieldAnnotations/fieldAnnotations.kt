987e78498bf278365f409b59da35b579518171e0
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 20:00:58 -0400

    Initialize abannykh/1.1.60/fix-mpp-js-test

---
+++ plugins/annotation-collector/testData/collectToFile/fieldAnnotations/fieldAnnotations.kt
@@ -0,0 +1,11 @@
+package org.test
+
+public class SomeClass {
+
+    @java.lang.Deprecated
+    public val annotatedVal: String? = null
+
+    @java.lang.Deprecated
+    public var annotatedVar: Int = 5
+
+}
