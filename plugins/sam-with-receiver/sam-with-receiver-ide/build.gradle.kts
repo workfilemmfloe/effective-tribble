b34b5ac49bfb371424350ca82ae38aef921fa4f6
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 21:15:08 -0400

    Initialize 1.2.30_171

---
+++ plugins/sam-with-receiver/sam-with-receiver-ide/build.gradle.kts
@@ -0,0 +1,27 @@
+
+description = "Kotlin SamWithReceiver IDEA Plugin"
+
+apply { plugin("kotlin") }
+
+jvmTarget = "1.6"
+
+dependencies {
+    compile(project(":kotlin-sam-with-receiver-compiler-plugin"))
+    compile(project(":plugins:annotation-based-compiler-plugins-ide-support"))
+    compile(project(":compiler:util"))
+    compile(project(":compiler:frontend"))
+    compile(project(":compiler:frontend.java"))
+    compile(project(":idea:idea-core"))
+    compile(project(":idea:idea-android"))
+    compile(project(":idea"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
+
+runtimeJar()
+
+ideaPlugin()
+
