9be69f30b902ff0d960108f6821db2dc88489f37
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 21:33:38 -0400

    Initialize master_171

---
+++ idea/idea-jps-common/build.gradle.kts
@@ -0,0 +1,21 @@
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compile(projectDist(":kotlin-stdlib"))
+    compileOnly(project(":kotlin-reflect-api"))
+    compile(project(":compiler:util"))
+    compile(project(":compiler:cli-common"))
+    compile(project(":compiler:frontend.java"))
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
+    compileOnly(intellijDep()) { includeJars("jdom", "util") }
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
+
