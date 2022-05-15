34e2d3269e7400e27a99f8c7cb8814c69aac7700
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 23:48:19 -0400

    Initialize icons-sync/8b78e566-c2ed-42f9-968f-7ab0346a0cbd

---
+++ plugins/kotlin-serialization/kotlin-serialization-compiler/build.gradle.kts
@@ -0,0 +1,34 @@
+
+description = "Kotlin Serialization Compiler Plugin"
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+jvmTarget = "1.6"
+
+dependencies {
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
+
+    compile(project(":compiler:plugin-api"))
+    compile(project(":compiler:frontend"))
+    compile(project(":compiler:backend"))
+    compile(project(":js:js.frontend"))
+    compile(project(":js:js.translator"))
+
+    runtime(project(":kotlin-stdlib"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
+
+val jar = runtimeJar {}
+
+dist(targetName = the<BasePluginConvention>().archivesBaseName + ".jar")
+
+ideaPlugin {
+    from(jar)
+}
