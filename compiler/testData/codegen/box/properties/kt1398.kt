af01dc9eafe3760797fd3d3864946e855fd340fb
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 23:24:56 -0400

    Initialize elizarov/coro-doc-fixes

---
+++ compiler/testData/codegen/box/properties/kt1398.kt
@@ -0,0 +1,11 @@
+// IGNORE_BACKEND: JS_IR
+// TODO: muted automatically, investigate should it be ran for JS or not
+// IGNORE_BACKEND: JS, NATIVE
+
+open class Base(val bar: String)
+
+class Foo(bar: String) : Base(bar) {
+  fun something() = (bar as java.lang.String).toUpperCase()
+}
+
+fun box() = Foo("ok").something()
