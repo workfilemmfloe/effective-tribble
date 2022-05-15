5ac7c319800c7e8670320312336f1058c6d10831
r/crtn32002 <71030478+workfilemmfloe@users.noreply.github.com>
2022-05-14 21:55:14 -0400

    Initialize 1.2.41

---
+++ js/js.translator/testData/box/expression/when/whenAsExpression.kt
@@ -0,0 +1,15 @@
+// IGNORE_BACKEND: JS_IR
+// EXPECTED_REACHABLE_NODES: 1108
+package foo
+
+
+fun box(): String {
+
+    val success = (when(1) {
+        2 -> 3
+        1 -> 1
+        else -> 5
+    } == 1)
+
+    return if (success) "OK" else "fail"
+}
---
+++ js/js.translator/testData/box/inlineEvaluationOrder/conditionalTestExpressionElvis.kt
@@ -0,0 +1,16 @@
+// IGNORE_BACKEND: JS_IR
+// EXPECTED_REACHABLE_NODES: 1119
+package foo
+
+fun test(x: Boolean?): Boolean = buzz(x) ?: fizz(true)
+
+fun box(): String {
+    assertEquals(true, test(null))
+    assertEquals("buzz(null);fizz(true);", pullLog())
+    assertEquals(false, test(false))
+    assertEquals("buzz(false);", pullLog())
+    assertEquals(true, test(true))
+    assertEquals("buzz(true);", pullLog())
+
+    return "OK"
+}
